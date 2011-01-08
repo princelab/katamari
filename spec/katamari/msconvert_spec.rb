5aa8c69b9ea2e891430ba70d9ee74808771f4242
John Prince <jtprince@gmail.com>
2011-01-07 23:27:22 -0700

    progress

--- spec/katamari/msconvert_spec.rb
+++ spec/katamari/msconvert_spec.rb
@@ -1,20 +1,19 @@
 require 'spec_helper'
+require 'socket'
 require 'fileutils'
 
+TEST_PORT = 33424
+TEST_IP_ADDRESS = "127.0.0.1"
 
-$HAS_MSCONVERT_SERVER = 
-  begin
-    server = "192.168.101.185"
-    port = 22007
-    !TCPSocket.new(server, port).nil?
-  rescue Errno::ETIMEDOUT
-    false
-  end
+### CREATE 
 
-MOUNT_POINT = "#{ENV["HOME"]/lab}"
-NON_MOUNT_DIR = "#{ENV["HOME"]/tmp}"
+create a TCP Server and have it spit back what the tester wants to hear (given the right input)
+
 
 describe 'converting raw files client/server' do
+  before do
+    TCPSocket.open(PORT)
+  end
   if $HAS_MSCONVERT_SERVER
     it 'converts raw files under the mount' do
       FileUtils.cp(TESTFILES + '/test.raw', MOUNT_POINT + '/tmp')       
